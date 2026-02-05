fb0783611c3bb5eb42f8bce0417b946249f3c757
Vignesh M <110014168+VigneshM-96@users.noreply.github.com>
2026-02-05 19:07:30 +0530

    Delete backend directory

--- backend/ml/preprocess.py
+++
@@ -1,6 +0,0 @@
-from sklearn.preprocessing import LabelEncoder
-
-def encode_location(df):
-    encoder = LabelEncoder()
-    df["Location"] = encoder.fit_transform(df["Location"])
-    return df, encoder
